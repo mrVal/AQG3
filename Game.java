2af5f4d5b4bdbf8448ffbe3076d56fbdcf07416b
Valeriy <valeriy.bartenev@gmail.com>
2017-10-17 13:38:27 +0300

    Initialize repository

---
+++ Game.java
@@ -0,0 +1,130 @@
+package com.angrygoose.aqg3;
+
+import com.angrygoose.aqg3.resources.Assets;
+import com.angrygoose.aqg3.rooms.Room0;
+import com.angrygoose.aqg3.rooms.Room;
+import com.angrygoose.aqg3.rooms.RoomManager;
+import com.angrygoose.aqg3.states.AdventureState;
+import com.angrygoose.aqg3.states.CombatState;
+import com.angrygoose.aqg3.states.State;
+import com.angrygoose.aqg3.states.StateManager;
+import com.angrygoose.aqg3.utilities.CoreFramework;
+import com.angrygoose.aqg3.utilities.Debugger;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author BallsofSteel
+ */
+public class Game extends CoreFramework{
+    
+    Handler handler;
+    
+    //States
+    protected StateManager stateManager;
+    private State adventureState;
+    private State combatState;
+    
+    //Rooms
+    RoomManager roomManager;
+    Room room0;
+
+    
+    Debugger debugger;
+    
+    @Override
+    protected void initializeGame(){
+        super.initializeGame();
+        debugger = new Debugger("Hello");
+        handler = new Handler(this);
+        handler.setKeyboard(keyboard);
+        handler.setMouse(mouse);
+        handler.setDebugger(debugger);
+        Assets.init();
+        
+        //States
+        /*
+        * When adding a state to the Map of states always use put() 
+        * with .getClass().getSimpleName() to make sure that 
+        * the map key coincides with the class name
+        */
+        Map states = new HashMap<String,State>();
+        adventureState = new AdventureState();
+        combatState = new CombatState();
+        states.put(adventureState.getClass().getSimpleName(), adventureState);
+        states.put(combatState.getClass().getSimpleName(), combatState);
+        stateManager = new StateManager(states);
+        stateManager.setState("AdventureState");
+        
+        //rooms
+        
+        room0 = new Room0(handler);
+        
+        /*
+        * When adding a room to the Map of rooms always use put() 
+        * with .getClass().getSimpleName() to make sure that 
+        * the map key coincides with the class name
+        */
+        Map rooms = new HashMap<String, Room>();
+        rooms.put(room0.getClass().getSimpleName(), room0);
+
+        roomManager = new RoomManager(rooms);
+    }
+    
+    @Override
+    protected void processInput(){
+        super.processInput();
+        if(keyboard.keyDownOnce(KeyEvent.VK_1)){
+            StateManager.setState("AdventureState");
+            handler.getDebugger().setText("adventureState");
+        }
+        if(keyboard.keyDownOnce(KeyEvent.VK_2)){
+            StateManager.setState("CombatState");
+            handler.getDebugger().setText("combatState");
+        }
+        
+        RoomManager.getCurrentRoom().processInput();
+    }
+    
+    @Override
+    protected void updateObjects(){
+        super.updateObjects();
+        
+        if(StateManager.getState() != null){
+            StateManager.getState().updateObjects();
+        }
+        
+        RoomManager.getCurrentRoom().updateObjects();
+    }
+    
+    @Override
+    protected void renderStuff(Graphics g){
+        //g.drawImage(Assets.hillsBgr, 0, 0, null);
+        super.renderStuff(g);
+        g.drawString("I'm alive", 200, 50);
+        
+        if(StateManager.getState() != null){
+            StateManager.getState().render(g);
+        }
+        
+        RoomManager.getCurrentRoom().render(g);
+        
+        System.out.println("current room: " + RoomManager.getCurrentRoom().toString());
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        Game game = new Game();
+        
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+               game.createAndShowGUI(); 
+            }
+        }); 
+    }  
+}
