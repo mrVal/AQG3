d36150238f4977c1a528c39c1008934a9426d1e3
Valeriy <valeriy.bartenev@gmail.com>
2017-10-31 08:47:29 +0200

    Add Resources class

---
+++ resources/Assets.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.angrygoose.aqg3.resources;
+
+import com.angrygoose.aqg3.utilities.Utility;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+/**
+ *
+ * @author BallsofSteel
+ */
+public class Assets {
+    //backgrounds
+    public static BufferedImage storyBgr, highlandsBgr, hillsBgr; 
+    
+    public static void init(){
+        
+        //storyInterface
+        storyBgr = Utility.loadImage(new File("res/img/storyBgr.png"));
+        
+        //backgrounds
+        highlandsBgr = Utility.loadImage(new File("res/img/highlandsBgr.png"));
+        hillsBgr = Utility.loadImage(new File("res/img/hillsBgr.png"));
+    }
+}
